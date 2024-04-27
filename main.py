ae57cef23029e84cc60a47c4852d90537a045afc
Mefamex <131618733+Mefamex@users.noreply.github.com>
2024-04-27 23:06:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+print("\n\n --- CURRENT PATH:\n \t\t",os.getcwd())
 from moviepy.editor import *
-video = VideoFileClip("aaa.mp4")
+video = VideoFileClip("a.mp4")
 video.audio.write_audiofile("ab.mp3")
